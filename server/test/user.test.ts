a62b475c9025669ffeb64abb4664fdcfdb1d6e58
robmal-0 <robmal-0@student.ltu.se>
2023-12-07 15:15:36 +0100

    connect

--- server/test/user.test.ts
+++ server/test/user.test.ts
@@ -1,14 +1,24 @@
 import { test, beforeAll, expect, afterAll } from 'bun:test'
 import Server from '../src/Server'
+import userRouter from '../src/routes/user'
 
 let port: number
 beforeAll(async () => {
-	await new Promise<void>((resolve) => {
+	await new Promise<void>((resolve, reject) => {
 		// eslint-disable-next-line no-new
 		new Server(3000, 'mysql://admin:pwd123@db_test:3306/db_test', (server: Server) => {
 			port = server.port
 			console.log(`Test server running at port ${port}`)
-			resolve()
+			server.db.authenticate().then(() => {
+				console.log('Database up and running')
+				// server.server.use('/user', userRouter)
+				server.server.get('/test', (req, res) => { res.send('Hello!') })
+				resolve()
+			}).catch(() => {
+				const msg = 'Failed to connect to database'
+				console.log(msg)
+				reject(msg)
+			})
 		})
 	})
 })
