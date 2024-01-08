35b1946db003421335510579c35f9202904b3dae
soderandreas <anesed-0@student.ltu.se>
2024-01-08 12:12:50 +0100

    added use of username in route for banned

--- server/src/routes/user/banned.ts
+++ server/src/routes/user/banned.ts
@@ -1,20 +1,30 @@
 import express from 'express'
 import BannedUser from '../../models/BannedUser'
 import { requireAdmin } from '../../utils/auth_utils'
+import User from '../../models/User'
 
 const banRouter = express.Router()
 
 // get info about one specific banned user
-banRouter.get('/:uId/banned/', requireAdmin(), (req, res) => {
+banRouter.get('/:username/banned/', requireAdmin(), (req, res) => {
 	// add check user is admin
 
-	BannedUser.findAll({
-		where: {
-			userId: req.params.uId
+	BannedUser.findOne({
+		include: {
+			model: User,
+			where: {
+				username: req.params.username
+			},
+			attributes: {
+				exclude: ['password', 'userType']
+			}
+		},
+		attributes: {
+			exclude: ['userId']
 		}
 	})
 		.then((found) => {
-			if (found[0] !== undefined) {
+			if (found !== null) {
 				res.status(200)
 				res.send(found)
 			} else {
@@ -31,9 +41,18 @@ banRouter.get('/:uId/banned/', requireAdmin(), (req, res) => {
 
 // get list of all banned users
 banRouter.get('/banned/', requireAdmin(), (req, res) => {
-	// add check user is admin
+	// add check user is admin, done
 
 	BannedUser.findAll({
+		include: {
+			model: User,
+			attributes: {
+				exclude: ['password', 'userType']
+			}
+		},
+		attributes: {
+			exclude: ['userId']
+		}
 	})
 		.then((found) => {
 			if (found !== null) {
@@ -51,75 +70,118 @@ banRouter.get('/banned/', requireAdmin(), (req, res) => {
 		})
 })
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-banRouter.post('/:uId/banned/', requireAdmin(), async (req, res) => {
-	BannedUser?.findOrCreate({
+banRouter.post('/:username/banned/', requireAdmin(), (req, res) => {
+	// check that user is admin, done
+
+	User.findOne({
 		where: {
-			userId: req.params.uId,
-			unbanDate: req.body.unbanDate
+			username: req.params.username
 		}
 	})
-		.then(([user, created]) => {
-			if (created) {
-				res.status(201)
-				res.send('Successfully banned user until ' + req.body.unbanDate)
-			} else {
-				res.status(200)
-				res.send('User already banned until ' + req.body.unbanDate)
-			}
+		.then((result) => {
+			BannedUser?.findOrCreate({
+				where: {
+					userId: result?.dataValues.id,
+					unbanDate: req.body.unbanDate
+				}
+			})
+				.then(([user, created]) => {
+					if (created) {
+						res.status(201)
+						res.send('Successfully banned user until ' + req.body.unbanDate)
+					} else {
+						res.status(200)
+						res.send('User already banned until ' + req.body.unbanDate)
+					}
+				})
+				.catch((e) => {
+					console.error(e)
+					res.status(500)
+					res.send('Failed to ban user')
+				})
 		})
 		.catch((e) => {
 			console.error(e)
 			res.status(500)
-			res.send('Failed to ban user')
+			res.send('Failed to get information about user')
 		})
 })
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-banRouter.delete('/:uId/banned/', requireAdmin(), async (req, res) => {
-	// check if user is logged in
-	// check if user is an admin
+banRouter.delete('/:username/banned/', requireAdmin(), (req, res) => {
+	// check if user is an admin, done
 
-	BannedUser.destroy({
-		where: { userId: req.params.uId }
+	User.findOne({
+		where: {
+			username: req.params.username
+		}
 	})
 		.then((result) => {
-			if (result === 1) {
-				res.status(200)
-				res.send('User successfully unbanned')
+			if (result !== null) {
+				BannedUser.destroy({
+					where: { userId: result?.dataValues.id }
+				})
+					.then((result) => {
+						if (result === 1) {
+							res.status(200)
+							res.send('User successfully unbanned')
+						} else {
+							res.status(404)
+							res.send('User could not be found among banned users')
+						}
+					})
+					.catch((e) => {
+						console.error(e)
+						res.status(500)
+						res.send('Failed to unban user')
+					})
 			} else {
 				res.status(404)
-				res.send('User could not be found among banned users')
+				res.send('User could not be found')
 			}
 		})
 		.catch((e) => {
 			console.error(e)
 			res.status(500)
-			res.send('Failed to unban user')
+			res.send('Failed to get information about user')
 		})
 })
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-banRouter.patch('/:uId/banned/', requireAdmin(), async (req, res) => {
-	// check if user is logged in
-	// check if user is an admin
+banRouter.patch('/:username/banned/', requireAdmin(), (req, res) => {
+	// check if user is an admin, done
 
-	BannedUser.update(req.body, {
-		where: { userId: req.params.uId }
+	User.findOne({
+		where: {
+			username: req.params.username
+		}
 	})
-		.then((saved) => {
-			if (saved[0] === 1) {
-				res.status(200)
-				res.send('Ban information was updated')
+		.then((result) => {
+			if (result !== null) {
+				BannedUser.update(req.body, {
+					where: { userId: result?.dataValues.id }
+				})
+					.then((saved) => {
+						if (saved[0] === 1) {
+							res.status(200)
+							res.send('Ban information was updated')
+						} else {
+							res.status(404)
+							res.send('Ban information could not be found')
+						}
+					})
+					.catch((e) => {
+						console.error(e)
+						res.status(500)
+						res.send('Failed to update information about banned user')
+					})
 			} else {
 				res.status(404)
-				res.send('Ban information could not be found')
+				res.send('User information could not be found')
 			}
 		})
 		.catch((e) => {
 			console.error(e)
 			res.status(500)
-			res.send('Failed to update information about banned user')
+			res.send('Failed to get information about user')
 		})
 })
 
